673d9748f98732a50e6f2ed7afbc7fbc2d566fbb
Home <renddi@yahoo.com>
2018-07-19 18:47:42 +0400

    Task_logical_operators_Task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Task_logical_operators taskLogicalOperators = new Task_logical_operators();
 
-        taskLogicalOperators.task1(1, 4, 3, 3);
+        //taskLogicalOperators.task1_2(1, 4, 3, 3);
+        taskLogicalOperators.task3(2,3,4,5,6);
     }
 }
--- src/Task_logical_operators.java
+++ src/Task_logical_operators.java
@@ -1,7 +1,7 @@
 public class Task_logical_operators {
     //1. Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
     //2. Вывести на консоль количество максимальных чисел среди этих четырех.
-    public int task1(int a,int b, int c,int d){
+    public int task1_2(int a, int b, int c, int d){
         int min = a;
         int max = a;
         int count = 0;
@@ -24,6 +24,32 @@ public class Task_logical_operators {
         System.out.println("Количество максимальных чисел: " + count);
 
         return min;
+    }
+    //3. Даны 5 чисел (тип int). Вывести вначале наименьшее, а затем самое большое из данных чисел.
+
+    public int task3(int a, int b, int c, int d, int e) {
+        int min = a;
+        int max = a;
+
+
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+        if (e < min) min = e;
+
+        if (max < b) max = b;
+        if (max < c) max = c;
+        if (max < d) max = d;
+        if (max < e) max = e;
+
+
+
+        System.out.println("Максимальное число: " + max);
+        System.out.println("Минимальное число: " + min);
+        
+
+        return min;
+
     }
 
 }
