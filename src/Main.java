9f207d3e63dbdd2952cf94bccbd51f962d0cb9a8
Home <renddi@yahoo.com>
2018-07-19 18:16:32 +0400

    Task_logical_operators_Task2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Task_logical_operators taskLogicalOperators = new Task_logical_operators();
 
-        taskLogicalOperators.task1(8, 2, 3, 4);
+        taskLogicalOperators.task1(1, 4, 3, 3);
     }
 }
--- src/Task_logical_operators.java
+++ src/Task_logical_operators.java
@@ -1,16 +1,29 @@
 public class Task_logical_operators {
     //1. Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
+    //2. Вывести на консоль количество максимальных чисел среди этих четырех.
     public int task1(int a,int b, int c,int d){
         int min = a;
+        int max = a;
+        int count = 0;
+
         if (b < min ) min = b;
         if (min > c) min =c;
         if (min > d) min = d;
 
-        System.out.println(min);
+        if (max < b) max = b;
+        if (max < c) max = c;
+        if (max < d) max = d;
 
+        if (max == a) count++;
+        if (max == b) count++;
+        if (max == c) count++;
+        if (max == d) count++;
+
+        System.out.println("Максимальное число: " + max);
+        System.out.println("Минимальное число: " + min);
+        System.out.println("Количество максимальных чисел: " + count);
 
         return min;
-
-
     }
+
 }
